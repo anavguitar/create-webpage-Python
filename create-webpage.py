e04cd020f0608b61fd4747ea786e79d543468c61
Andres Avendano <anavguitar@gmail.com>
2024-10-15 20:37:44 -0700

    fixed bugs when using the os module

--- create-webpage.py
+++ create-webpage.py
@@ -10,7 +10,7 @@ import datetime
 # get the user input
 
 file_name = input("Enter the name of the file (do not include .html extension):")
-out_file = open(f'{file_name}.html', 'a+')
+out_file = f'{file_name}.html'
 
 body_h1 = input('Enter the character name: ')
 body_h1 = f'<h1>{body_h1}</h1>\n'
@@ -24,19 +24,19 @@ body_description = f'<p>{body_description}</p>\n'
 body_img = input('Enter the location/path of the character\'s image: ')
 body_img = f'<img src={body_img} alt="{file_name}">\n'
 
-combined_body = f"""
-    {body_h1}
-    {body_h2}
-    {body_description}
-    {body_img}
-"""
-
-test = open('test.txt', 'r')
-print(test.read())
+combined_body = body_h1 + body_h2 + body_description + body_img
 
 page_head = open("page-head.txt", "r")
+combined_head = ''.join(page_head.readlines())
+page_head.close()
+
 page_footer = open("page-footer.txt", "r")
+combined_footer = ''.join(page_footer.readlines())
+page_footer.close()
 
-print(page_head.read())
-print(page_footer.read())
+combined_page = combined_head + combined_body + combined_footer
 
+f = open(out_file, 'w')
+f.write(combined_page)
+f.close()
+print("Created {}".format(out_file))
